ca7fd1b0134b10521a4ff08f86930a6f6e66f301
seeeeeeong <lsinsung@gmail.com>
2025-01-25 06:22:34 +0900

    test: UrlShortenService 테스트 코드 추가

---
+++ src/test/java/community/whatever/onembackendjava/urlshorten/UrlShortenServiceTest.java
@@ -0,0 +1,36 @@
+package community.whatever.onembackendjava.urlshorten;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenServiceTest {
+
+    @Autowired
+    private UrlShortenService urlShortenService;
+
+    @Test
+    @DisplayName("shorten-url을 생성하고 조회한다.")
+    void shortenUrlCreateAndSearch() {
+        String expectedOriginUrl = "https://www.google.com";
+        String key = urlShortenService.shortenUrlCreate(expectedOriginUrl);
+        String originUrl = urlShortenService.shortenUrlSearch(key);
+
+        assertTrue(originUrl.equals(expectedOriginUrl));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 key로 조회하면 예외가 발생한다.")
+    void shortenUrlSearchWithInvalidKey() {
+        String invalidKey = "invalidKey";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            urlShortenService.shortenUrlSearch(invalidKey);
+        });
+    }
+
+}
