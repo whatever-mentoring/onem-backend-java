2b6a09afe86fe9f5847f3af7f265f8fa2159a5a3
kyungmin <wktls63@naver.com>
2025-01-25 02:49:45 +0900

    test: 문자열 값 Value 주입 방식으로 변경

--- src/test/java/community/whatever/onembackendjava/OnemBackendJavaApplicationTests.java
+++ src/test/java/community/whatever/onembackendjava/OnemBackendJavaApplicationTests.java
@@ -2,15 +2,18 @@ package community.whatever.onembackendjava;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.http.HttpStatus;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class UrlShortenControllerTest {
 
     @Autowired
@@ -19,12 +22,21 @@ class UrlShortenControllerTest {
     @LocalServerPort
     private int port;
 
+    @Value("${shortener.endpoint.create}")
+    private String createEndpoint;
+
+    @Value("${shortener.endpoint.search}")
+    private String searchEndpoint;
+
+    @Value("${shortener.origin}")
+    private String originUrl;
+
+    @Value("${shortener.invalid-key}")
+    private String invalidKey;
+
     @Test
     void createShortenUrlTest() {
-
-        String originUrl = "https://www.google.com";
-
-        ResponseEntity<String> response = restTemplate.postForEntity("/shorten-url/create", originUrl, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(createEndpoint, originUrl, String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         String generatedKey = response.getBody();
@@ -33,11 +45,9 @@ class UrlShortenControllerTest {
 
     @Test
     void searchShortenUrlTest() {
-        String originUrl = "https://www.google.com";
+        String generatedKey = restTemplate.postForObject(createEndpoint, originUrl, String.class);
 
-        String generatedKey = restTemplate.postForObject("/shorten-url/create", originUrl, String.class);
-
-        ResponseEntity<String> response = restTemplate.postForEntity("/shorten-url/search", generatedKey, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(searchEndpoint, generatedKey, String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(originUrl);
@@ -45,9 +55,8 @@ class UrlShortenControllerTest {
 
     @Test
     void searchShortenUrlInvalidKeyTest() {
-        String invalidKey = "9999";
 
-        ResponseEntity<String> response = restTemplate.postForEntity("/shorten-url/search", invalidKey, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(searchEndpoint, invalidKey, String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
