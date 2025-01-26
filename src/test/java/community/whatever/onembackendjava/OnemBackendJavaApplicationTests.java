e1de782e97cd7a5a9dc0e9bc177b6aaf77503f79
kyungmin <wktls63@naver.com>
2025-01-26 20:02:22 +0900

    refactor: 메서드, 클래스 네이밍 개선 및 파라미터명 수정

--- src/test/java/community/whatever/onembackendjava/OnemBackendJavaApplicationTests.java
+++ src/test/java/community/whatever/onembackendjava/OnemBackendJavaApplicationTests.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-class UrlShortenControllerTest {
+class ShortUrlControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
