b118141954c34f47c51062c1656ee86dc1345225
kyungmin <wktls63@naver.com>
2025-01-26 19:36:56 +0900

    chor: service 디렉토리 생성 및 UrlShortenService 클래스 추가

---
+++ src/main/java/community/whatever/onembackendjava/service/UrlShortenService.java
@@ -0,0 +1,17 @@
+package community.whatever.onembackendjava.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlShortenService {
+
+    public String createShortenUrl(String originUrl) {
+
+        return null;
+    }
+
+    public String getOriginUrl(String key) {
+
+        return null;
+    }
+}
