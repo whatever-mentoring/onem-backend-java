d70f58164f921ab1515988a7c0c75e9110b41ce3
judangs <wneotjd228@naver.com>
2025-01-26 17:41:00 +0900

    refactor: 엔드포인트 수정

--- src/main/java/community/whatever/onembackendjava/controller/UrlShortenController.java
+++ src/main/java/community/whatever/onembackendjava/controller/UrlShortenController.java
@@ -3,7 +3,7 @@ package community.whatever.onembackendjava.controller;
 import community.whatever.onembackendjava.service.UrlShorteningService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,12 +13,12 @@ public class UrlShortenController {
     private final UrlShorteningService urlShorteningService;
 
     @PostMapping("/shorten-url/search")
-    public String shortenUrlSearch(@RequestBody String key) {
+    public String shortenUrlSearch(@RequestParam String key) {
         return urlShorteningService.searchOriginUri(key);
     }
 
     @PostMapping("/shorten-url/create")
-    public String shortenUrlCreate(@RequestBody String originUrl) {
+    public String shortenUrlCreate(@RequestParam String originUrl) {
         return urlShorteningService.createShortenUri(originUrl);
     }
 }
