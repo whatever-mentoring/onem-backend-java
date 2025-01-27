64db13005656f2682596955c8c3e750b538b3d54
judangs <wneotjd228@naver.com>
2025-01-27 22:26:18 +0900

    reafactor: Random 객체 생성 방식 수정

--- src/main/java/community/whatever/onembackendjava/repository/ShortenUrlRepository.java
+++ src/main/java/community/whatever/onembackendjava/repository/ShortenUrlRepository.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ShortenUrlRepository {
 
     private final Map<String, String> storage = new ConcurrentHashMap<>();
+    private final Random random = new Random();
 
     public String read(String id) throws IllegalArgumentException {
         if (!storage.containsKey(id)) {
@@ -20,7 +21,7 @@ public class ShortenUrlRepository {
     }
 
     public String store(String originUrl) {
-        String id = String.valueOf(new Random().nextInt(10000));
+        String id = String.valueOf(random.nextInt(10000));
         storage.put(id, originUrl);
         return id;
     }
