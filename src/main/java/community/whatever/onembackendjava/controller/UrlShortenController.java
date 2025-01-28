c2f4ced5f3301ed50a058ead9604233c4fa0bb45
nowiz <moonone470@gmail.com>
2025-01-28 19:06:00 +0900

    fix: API 경로 및 메서드 수정
    전체적인 API 설계(경로, 메서드, 응답 방식 등)를 RESTful하게 개선

--- src/main/java/community/whatever/onembackendjava/controller/UrlShortenController.java
+++ src/main/java/community/whatever/onembackendjava/controller/UrlShortenController.java
@@ -2,6 +2,7 @@ package community.whatever.onembackendjava.controller;
 
 import community.whatever.onembackendjava.service.UrlShortenService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,4 +12,15 @@ public class UrlShortenController {
 
     private final UrlShortenService urlShortenService;
 
+    @GetMapping("/search/{urlKey}")
+    public ResponseEntity<String> getShortUrl(@PathVariable("urlKey") String urlKey) {
+        String originalUrl = urlShortenService.getShortUrl(urlKey);
+        return ResponseEntity.ok(originalUrl);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createShortUrl(@RequestBody String originUrl) {
+        String shortenUrl = urlShortenService.createShortUrl(originUrl);
+        return ResponseEntity.ok(shortenUrl);
+    }
 }
