ad1cb47d120dbb35d99bb6cec4a75d25d3f48c1c
JianYang98 <daibobby1221@gmail.com>
2025-01-30 00:02:15 +0900

    내용 : 멘토님이 풀리퀘스트에서,,, 제안해주신 부분 ,,, 실수로 누락하고 머지해서 직접 코드 수정 ★ 개발자 :양지안

--- src/main/java/community/whatever/onembackendjava/exception/GlobalExceptionHandler.java
+++ src/main/java/community/whatever/onembackendjava/exception/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package community.whatever.onembackendjava.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({ IllegalArgumentException.class})
-    public ResponseEntity<Object> IllegalArgumentException(IllegalArgumentException ex){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage()) ;
+    @ExceptionHandler({IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object IllegalArgumentException(IllegalArgumentException ex) {
+        return ex.getMessage();
     }
 }
