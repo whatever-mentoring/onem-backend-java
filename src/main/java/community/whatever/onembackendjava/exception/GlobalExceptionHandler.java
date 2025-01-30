0c713be244258bdf570eed85597f0ede924a5ac7
nowiz <moonone470@gmail.com>
2025-01-30 17:04:33 +0900

    feat: 전역 예외 처리 추가
    IllegalArgumentException 발생 시, 커스텀 응답 반환

--- src/main/java/community/whatever/onembackendjava/exception/GlobalExceptionHandler.java
+++ src/main/java/community/whatever/onembackendjava/exception/GlobalExceptionHandler.java
@@ -4,13 +4,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex){
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "INVALID_ARGUMENT");
+        errorResponse.put("message", ex.getMessage());
 
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
 }
