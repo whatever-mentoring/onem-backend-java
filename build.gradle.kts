59bc62fc9ea6d9eeb4962995751bd0e2fbdfaf2a
kyungmin <wktls63@naver.com>
2025-02-01 22:13:57 +0900

    chore: Spring REST Docs 의존성 추가, 빌드 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
+
 plugins {
     java
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.asciidoctor.jvm.convert") version "4.0.0"
 }
 
 group = "community.whatever"
@@ -18,12 +21,31 @@ repositories {
 }
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+
+val snippetsDir = file("build/generated-snippets")
+
 tasks.withType<Test> {
     useJUnitPlatform()
+    outputs.dir(snippetsDir)
+}
+
+
+
+tasks.named<AsciidoctorTask>("asciidoctor") {
+    dependsOn(tasks.test)
+    inputs.dir(snippetsDir)
+
+    baseDirFollowsSourceFile()
 }
