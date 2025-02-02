489ddc6c013e9067b21b36881e39ba042144d51d
JianYang98 <daibobby1221@gmail.com>
2025-02-03 07:02:44 +0900

    내용 :  RestAssured dependencies 제거 개발자 :양지안

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     compileOnly  ("org.projectlombok:lombok")
     annotationProcessor ("org.projectlombok:lombok")
-    testImplementation ("io.rest-assured:rest-assured")
 }
 
 tasks.withType<Test> {
