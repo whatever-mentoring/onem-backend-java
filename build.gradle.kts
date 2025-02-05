1eda56d347b4ae02c82106777f862b55c432a99a
seeeeeeong <lsinsung@gmail.com>
2025-02-06 04:51:36 +0900

    refactor: blacklist yml 파일에서 관리하도록 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- src/main/java/community/whatever/onembackendjava/service/UrlShortenService.java
+++ src/main/java/community/whatever/onembackendjava/service/UrlShortenService.java
@@ -3,21 +3,23 @@ package community.whatever.onembackendjava.service;
 import community.whatever.onembackendjava.common.exception.ErrorCode;
 import community.whatever.onembackendjava.common.exception.custom.NotFoundException;
 import community.whatever.onembackendjava.component.UrlShortener;
-import community.whatever.onembackendjava.component.UrlValidator;
 import community.whatever.onembackendjava.repository.UrlShortenRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlShortenService {
 
+    private final ValidateUrlShortenService validateUrlShortenService;
     private final UrlShortenRepository urlShortenRepository;
 
-    public UrlShortenService(UrlShortenRepository urlShortenRepository) {
+    public UrlShortenService(ValidateUrlShortenService validateUrlShortenService,
+        UrlShortenRepository urlShortenRepository) {
+        this.validateUrlShortenService = validateUrlShortenService;
         this.urlShortenRepository = urlShortenRepository;
     }
 
     public String createShortenUrl(String originUrl) {
-        UrlValidator.validateUrl(originUrl);
+        validateUrlShortenService.validateUrl(originUrl);
         String shortenUrlKey = UrlShortener.shorten();
         urlShortenRepository.save(shortenUrlKey, originUrl);
         return shortenUrlKey;
