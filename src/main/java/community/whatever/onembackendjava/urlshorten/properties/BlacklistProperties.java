e11535bc5f7d9adaf7e24ef735c102df759edff5
seeeeeeong <lsinsung@gmail.com>
2025-02-07 18:47:32 +0900

    refactor: BlacklistProperties에서 @RefreshScope 제거

--- src/main/java/community/whatever/onembackendjava/urlshorten/properties/BlacklistProperties.java
+++ src/main/java/community/whatever/onembackendjava/urlshorten/properties/BlacklistProperties.java
@@ -3,10 +3,8 @@ package community.whatever.onembackendjava.urlshorten.properties;
 
 import java.util.Set;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
-@RefreshScope
 @Configuration
 @ConfigurationProperties("blacklist")
 public class BlacklistProperties {
