3cc1df7285d96ec2f22cd65acc08d2d7ea16db63
seeeeeeong <lsinsung@gmail.com>
2025-02-09 19:47:05 +0900

    refacotr: yml 설정 변경

--- src/main/java/community/whatever/onembackendjava/urlshorten/properties/ShortenUrlProperties.java
+++ src/main/java/community/whatever/onembackendjava/urlshorten/properties/ShortenUrlProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "urlshorten")
+@ConfigurationProperties(prefix = "shorten-url")
 public class ShortenUrlProperties {
 
     private int expirationTime;
