80e00096f2691fe5f76a105233fd939e387c7e9a
JianYang98 <daibobby1221@gmail.com>
2025-02-11 00:34:26 +0900

    내용 : 2-4 차단 도메인 레파지토리 추가 개발자 :양지안

--- src/main/java/community/whatever/onembackendjava/url/UrlShortenRepository.java
+++ src/main/java/community/whatever/onembackendjava/url/UrlShortenRepository.java
@@ -1,24 +1,39 @@
 package community.whatever.onembackendjava.url;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 @Slf4j
 @Repository
 public class UrlShortenRepository {
     private final Map<String, String> shortenUrlMap = new HashMap<>();
+
     private static final Random random = new Random() ;
+
+    private UrlBlockDomainProperties urlBlockDomainProperties ;
+
+    public UrlShortenRepository(UrlBlockDomainProperties urlBlockDomainProperties){
+        this.urlBlockDomainProperties = urlBlockDomainProperties ;
+    }
 
     boolean existKey(String key){
         return shortenUrlMap.containsKey(key) ;
     }
     public String createKey(String url){
-        String uniqueKey = generateKey();
-        shortenUrlMap.put(uniqueKey, url) ;
-        return uniqueKey ;
+
+        if(isblockedDomains(url)){
+            throw new IllegalArgumentException("blocked Domain");
+        }else{
+            String uniqueKey = generateKey();
+            shortenUrlMap.put(uniqueKey, url) ;
+            return uniqueKey ;
+        }
+
     }
 
     public String getUrl(String key){
@@ -33,4 +48,10 @@ public class UrlShortenRepository {
     private static String generateKey(){
         return  String.valueOf(random.nextInt(10000));
     }
+
+    public boolean isblockedDomains(String url){
+        List<String> blockedDomains = urlBlockDomainProperties.getBlockedDomains() ;
+        return blockedDomains.contains(url) ;
+    }
+
 }
