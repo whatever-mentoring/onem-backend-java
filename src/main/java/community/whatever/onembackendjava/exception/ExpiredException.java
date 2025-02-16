9290a7e89e5ed19f1cec6881ac1cdba4fe7eaa2f
JianYang98 <daibobby1221@gmail.com>
2025-02-16 22:54:54 +0900

    내용 : ExpiredException 수정 개발자 :양지안

--- src/main/java/community/whatever/onembackendjava/exception/ExpiredException.java
+++ src/main/java/community/whatever/onembackendjava/exception/ExpiredException.java
@@ -12,8 +12,5 @@ public class ExpiredException  extends RuntimeException {
     public ExpiredException(String message, Throwable cause) {
         super(message, cause);
     }
-    // Throwable을 직접 받는 생성자
-    public ExpiredException(Throwable cause) {
-        super(cause);
-    }
+
 }
