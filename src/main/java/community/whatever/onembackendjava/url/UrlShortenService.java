45ba90afd7d20d924aec9d0d6e3dca3a0bd24047
JianYang98 <daibobby1221@gmail.com>
2025-02-17 23:22:09 +0900

    내용 : 로직 분리 ( Repository는 영속성만 남기기) 개발자 :양지안

--- src/main/java/community/whatever/onembackendjava/url/UrlShortenService.java
+++ src/main/java/community/whatever/onembackendjava/url/UrlShortenService.java
@@ -1,38 +1,59 @@
 package community.whatever.onembackendjava.url;
 
+import community.whatever.onembackendjava.common.ShortenUrlKeyGenerator;
 import community.whatever.onembackendjava.exception.ExpiredException;
+import community.whatever.onembackendjava.expiration.ExpirationChecker;
+import community.whatever.onembackendjava.expiration.TimeBasedExpirationChecker;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class UrlShortenService {
 
     private  UrlShortenRepository urlShortenRepository ;
+    private  UrlShortenProperties urlShortenProperties ;
+    private  ShortenUrlKeyGenerator shortenUrlKeyGenerator ;
+    private ExpirationChecker expirationCheker;
 
-    public UrlShortenService(UrlShortenRepository urlShortenRepository ) {
+    public UrlShortenService(UrlShortenRepository urlShortenRepository , UrlShortenProperties urlShortenProperties
+            , ShortenUrlKeyGenerator shortenUrlKeyGenerator , ExpirationChecker timeExpirationChecker) {
         this.urlShortenRepository = urlShortenRepository;
-    }
+        this.shortenUrlKeyGenerator = shortenUrlKeyGenerator ;
+        this.urlShortenProperties =  urlShortenProperties ;
+        this.expirationCheker = timeExpirationChecker ;
 
+    }
 
     public String getUrlByKey(String key) {
         if(!urlShortenRepository.existKey(key)){
             throw new IllegalArgumentException("Invalid key");
         }
-
         ShortenUrl shortenUrl = urlShortenRepository.getShotenUrl(key) ;
-        if ( LocalDateTime.now().isAfter(shortenUrl.expirationTime())) {
+        if (expirationCheker.isExpired(LocalDateTime.now())) {
             throw new ExpiredException("currentKey expired");
         }
-
         return shortenUrl.originUrl() ;
-
-
     }
-
-
 
     public String createKey(String url) {
-        return  urlShortenRepository.createKey(url);
+
+        if (isblockedDomains(url)) {
+            throw new IllegalArgumentException("blocked Domain");
+        }
+        // 만료시간 세팅
+        LocalDateTime settingTime =LocalDateTime.now().plusMinutes(urlShortenProperties.getExpiredMinute());
+
+        String uniqueKey = shortenUrlKeyGenerator.generateKey();
+        ShortenUrl shortenUrl = new ShortenUrl(uniqueKey,url, settingTime)  ;
+        urlShortenRepository.saveKey(shortenUrl);
+        return  uniqueKey;
     }
+
+    public boolean isblockedDomains(String url){
+        List<String> blockedDomains = urlShortenProperties.getBlockedDomains() ;
+        return blockedDomains.contains(url) ;
+    }
+
 }
