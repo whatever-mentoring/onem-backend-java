51a329b0c899dc146ac1f8760e1697212e57277e
JianYang98 <daibobby1221@gmail.com>
2025-02-17 23:19:28 +0900

    내용 : 만료테스트 로직 분리 개발자 :양지안

---
+++ src/main/java/community/whatever/onembackendjava/expiration/FakeExpirationChecker.java
@@ -0,0 +1,18 @@
+package community.whatever.onembackendjava.expiration;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+public class FakeExpirationChecker implements ExpirationChecker {
+    private final boolean expired;
+
+    public FakeExpirationChecker(boolean expired) {
+        this.expired = expired;
+    }
+
+    @Override
+    public boolean isExpired(LocalDateTime expirationTime) {
+        return expired;
+    }
+}
