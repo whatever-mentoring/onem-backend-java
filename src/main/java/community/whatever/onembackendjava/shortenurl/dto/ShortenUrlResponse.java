728ff9037f0c8e18ecd51e136550cd83c9b147d5
seeeeeeong <lsinsung@gmail.com>
2025-02-18 10:52:14 +0900

    refactor: ShortenUrlResponse

--- src/main/java/community/whatever/onembackendjava/shortenurl/dto/ShortenUrlResponse.java
+++ src/main/java/community/whatever/onembackendjava/shortenurl/dto/ShortenUrlResponse.java
@@ -1,19 +1,19 @@
 package community.whatever.onembackendjava.shortenurl.dto;
 
 import community.whatever.onembackendjava.shortenurl.entity.ShortenUrl;
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record ShortenUrlResponse (
     String originUrl,
     String shortenUrlKey,
-    LocalDateTime expiredAt
+    String expiredAt
 
 ){
     public static ShortenUrlResponse from(ShortenUrl shortenUrl) {
         return new ShortenUrlResponse(
             shortenUrl.getOriginUrl(),
             shortenUrl.getShortenUrlKey(),
-            shortenUrl.getExpiredAt()
+            shortenUrl.getExpiredAt().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"))
         );
     }
 }
