b4b73c2d5da513c911f20cc272090e2502debf70
JianYang98 <daibobby1221@gmail.com>
2025-02-20 00:56:43 +0900

    내용 : 클래스 이름 Util -> Provider로 변경에 따른 ShortenUrlKeyGenerator 변경 개발자 :양지안

--- src/main/java/community/whatever/onembackendjava/common/ShortenUrlKeyGenerator.java
+++ src/main/java/community/whatever/onembackendjava/common/ShortenUrlKeyGenerator.java
@@ -7,16 +7,16 @@ import java.util.Random;
 
 @Component
 public class ShortenUrlKeyGenerator {
-    private ProfileUtil profileUtil ;
+    private ProfileProvider profileProvider ;
 
-    public ShortenUrlKeyGenerator(ProfileUtil profileUtil) {
-        this.profileUtil = profileUtil;
+    public ShortenUrlKeyGenerator(ProfileProvider profileProvider) {
+        this.profileProvider = profileProvider;
     }
 
     private static final Random random = new Random() ;
 
     public String generateKey(){
-        return  profileUtil.getActiveProfile()+"_"+String.valueOf(random.nextInt(10000));
+        return  profileProvider.getActiveProfile()+"_"+String.valueOf(random.nextInt(10000));
     }
 
 
