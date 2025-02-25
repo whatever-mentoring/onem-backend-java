b4225b97c67cb20e6fa4fcd58fb3333967c8d88b
Sung-Heon <elon4856@gmail.com>
2025-02-25 22:11:56 +0900

    [feat] Swagger UI 설정 추가
    - SpringDoc OpenAPI 의존성 추가
    - Swagger UI 기본 설정 구현
    - API 문서화를 위한 기본 정보 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
