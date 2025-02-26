49b2f845f152f83e34e1b8d7dfebca5d77e6feaa
Sung-Heon <elon4856@gmail.com>
2025-02-26 19:44:37 +0900

    [feat] 2048자 validation 추가.

--- src/main/java/community/whatever/onembackendjava/dto/CreateShortenUrlRequest.java
+++ src/main/java/community/whatever/onembackendjava/dto/CreateShortenUrlRequest.java
@@ -1,5 +1,11 @@
 package community.whatever.onembackendjava.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreateShortenUrlRequest(
-    String originUrl
-) {}
+        @NotBlank(message = "URL이 비어있으면 안됩니다.")
+        @Size(max = 2048, message = "URL은 2048자를 넘을 수 없습니다.")
+        String originUrl
+) {
+}
