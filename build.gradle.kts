d9c73e046afaaf481bb28c939ddd8bbecbde5108
Sung-Heon <elon4856@gmail.com>
2025-02-27 10:12:23 +0900

    [fix] 롬복 버전 명시

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    compileOnly("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
 }
 
