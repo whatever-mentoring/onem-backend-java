ee46b792f936aeabcb922e677fd9bfed5594c2e1
ALbertIM0427 <guang7226961@naver.com>
2025-02-27 17:54:54 +0900

    chore: 테스트 옵션 제거
    전체 테스트에 제한을 두기보다는 제한이 필요한 테스트에만 실행 설정을 추가하는 방식이 더 적합하다고 판단하여 수정하였습니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,4 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-    // 테스트는 1 core에서만 실행하도록 설정하여 실제 운영 환경과 동일하게 테스트를 수행합니다.
-    maxParallelForks = 1
-    // 실제 운영환경이 물리 메모리가 1 GB이고 기본 JVM 메모리 설정이 1/4이므로 256 MB로 제한합니다.
-    jvmArgs("-Xmx256m", "-Xms256m")
 }
