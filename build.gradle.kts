a87df5b841297974ab12f52cf252523109ec6cb1
jiheon2234 <jiheon2234@gmail.com>
2025-02-27 20:20:30 +0900

    validation 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
 
     //lombok
