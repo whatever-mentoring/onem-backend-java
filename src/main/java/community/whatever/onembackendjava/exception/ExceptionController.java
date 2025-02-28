e40ec4411d1fbcd1e9cceb09f24f01000b4935a1
jiheon2234 <jiheon2234@gmail.com>
2025-02-28 20:08:58 +0900

    지적받은 것들 수정

--- src/main/java/community/whatever/onembackendjava/exception/ExceptionController.java
+++ src/main/java/community/whatever/onembackendjava/exception/ExceptionController.java
@@ -1,36 +1,34 @@
 package community.whatever.onembackendjava.exception;
 
-import java.util.HashMap;
-
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import community.whatever.onembackendjava.exception.res.BusinessExceptionResponse;
-import community.whatever.onembackendjava.exception.res.ValidationExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionController {
 
-	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(BusinessLogicException.class)
-	public BusinessExceptionResponse handleBusinessLogicException(BusinessLogicException e) {
-        log.error("error =", e);
-		return new BusinessExceptionResponse(e.getExceptionCode().getErrorMessage());
+	public ProblemDetail handleBusinessLogicException(BusinessLogicException e) {
+		log.error("error =", e);
+		var exceptionCode = e.getExceptionCode();
+		return ProblemDetail.forStatusAndDetail(exceptionCode.getStatus(), exceptionCode.getErrorMessage());
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidationExceptionResponse> handleMethodArgumentNotValidException(
+	public ProblemDetail handleMethodArgumentNotValidException(
 		MethodArgumentNotValidException e) {
-		HashMap<String, String> validations = new HashMap<>();
-		for (val fieldError : e.getFieldErrors()) {
-			validations.put(fieldError.getField(), fieldError.getDefaultMessage());
-		}
 
-		return ResponseEntity.badRequest().body(new ValidationExceptionResponse(validations));
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+		problemDetail.setTitle("필드 검증 오류");
+		FieldError fieldError = e.getFieldError();
+		problemDetail.setDetail(fieldError.getField() + " : " + fieldError.getDefaultMessage());
+
+		return problemDetail;
 	}
 }
