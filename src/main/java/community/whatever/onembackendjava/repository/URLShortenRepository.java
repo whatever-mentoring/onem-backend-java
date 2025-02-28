32fca9a05763ffc3ceb0230545a54a73d1273b77
jiheon2234 <jiheon2234@gmail.com>
2025-02-28 20:54:50 +0900

    문서화

--- src/main/java/community/whatever/onembackendjava/repository/URLShortenRepository.java
+++ src/main/java/community/whatever/onembackendjava/repository/URLShortenRepository.java
@@ -3,9 +3,21 @@ package community.whatever.onembackendjava.repository;
 import java.util.Optional;
 
 public interface URLShortenRepository {
+	/**
+	 * <p> 단축 URL로 저장되어 있는 원본 URL 조회</p>
+	 *
+	 * @param shortenedURL 단축 URL
+	 * @return 원본 URL
+	 */
+	Optional<String> findByShortenedURL(String shortenedURL);
 
-	Optional<String> findByShortenedURL(String key);
-
-	String create(String originURL, String shortenUrl);
+	/**
+	 * <p>원본 URL과 단축 URL을 저장</p>
+	 *
+	 * @param originURL    저장할 원본 URL
+	 * @param shortenedURL 저장할 단축 URL
+	 * @return 저장된 단축 URL
+	 */
+	String create(String originURL, String shortenedURL);
 
 }
--- src/main/java/community/whatever/onembackendjava/service/ShortenURLService.java
+++ src/main/java/community/whatever/onembackendjava/service/ShortenURLService.java
@@ -17,6 +17,13 @@ public class ShortenURLService {
 
 	private final Random random = new Random();
 
+	/**
+	 * <P> 단축 URL로 원본 URL 조회</P>
+	 *
+	 * @param shortenedURL 단축 URL
+	 * @return 원본 URL
+	 * @throws BusinessLogicException 원본 URL을 찾을 수 없는 경우
+	 */
 	public String getOriginURL(String shortenedURL) {
 		String originURL = repository.findByShortenedURL(shortenedURL)
 			.orElseThrow(() -> new BusinessLogicException(BusinessExceptionCode.ORIGIN_URL_NOT_FOUND));
@@ -24,6 +31,12 @@ public class ShortenURLService {
 		return originURL;
 	}
 
+	/**
+	 * <p> 원본 URL로 단축 URL 생성</p>
+	 *
+	 * @param originURL 원본 URL
+	 * @return 생성된 단축 URL
+	 */
 	public String createShortenedURL(String originURL) {
 		String generatedShortenedURL = generateShortenedURL();
 		String shortenedURL = repository.create(originURL, generatedShortenedURL);
@@ -31,6 +44,11 @@ public class ShortenURLService {
 		return shortenedURL;
 	}
 
+	/**
+	 * <p> 단축 URL 생성</p>
+	 *
+	 * @return 단축 URL
+	 */
 	private String generateShortenedURL() {
 		return String.valueOf(random.nextInt());
 	}
